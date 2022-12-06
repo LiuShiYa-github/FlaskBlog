73f2e40db39fd2e5732174cf4655b84334d7c28f
热气球 <55818804+LiuShiYa-github@users.noreply.github.com>
2022-12-06 21:12:27 +0800

    Add files via upload

---
+++ RealProject/settings.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DEBUG = True
+
+SECRET_KEY = 'l%3ya7fn3moipdpcltj(tdfcv5^@lj=t5d&72levvls+y*@_4^'
+
+SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@10.0.0.30:3306/flaskdb'
+
+SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+SQLALCHEMY_TRACK_MODIFICATIONS = True
