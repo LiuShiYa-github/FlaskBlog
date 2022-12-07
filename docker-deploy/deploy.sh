5089dd7465faa6c18353ea66a6d07452e794e8b8
热气球 <55818804+LiuShiYa-github@users.noreply.github.com>
2022-12-07 12:55:16 +0800

    Delete docker-deploy directory

--- docker-deploy/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd ../../ && mv FlaskBlog/docker-deploy/* .
-wget http://nginx.org/download/nginx-1.18.0.tar.gz
-chmod +x init.sh
-docker build -t  flaskblog:v1.0  ./
-docker network create --subnet=172.32.0.0/24 flaskblog_net
-docker-compose -f docker-compose.yaml up
