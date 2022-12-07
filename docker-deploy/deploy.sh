e8159cba53970efd218b5d089485f4957c595b99
热气球 <55818804+LiuShiYa-github@users.noreply.github.com>
2022-12-07 10:59:29 +0800

    Add files via upload

---
+++ docker-deploy/deploy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd ../../ && mv FlaskBlog/docker-deploy/* .
+wget http://nginx.org/download/nginx-1.18.0.tar.gz
+chmod +x init.sh
+docker build -t  flaskblog:v1.0  ./
+docker-compose -f docker-compose.yaml up
