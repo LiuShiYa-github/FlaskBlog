eb7132b3c9b254ca42a84d5cada52c545bbe290e
热气球 <55818804+LiuShiYa-github@users.noreply.github.com>
2022-12-07 14:42:13 +0800

    Update deploy.sh

--- docker-deploy/deploy.sh
+++ docker-deploy/deploy.sh
@@ -5,7 +5,7 @@ chmod +x init.sh
 docker build -t  flaskblog:v1.0  ./
 docker network create --subnet=172.32.0.0/24 flaskblog_net
 docker-compose -f docker-compose_mysql.yaml up -d
-sleep 3
+sleep 20
 docker exec -it mysql mysql -uroot -p123456 -e "use mysql; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '123456' ; flush privileges; create database flaskdb;"
 docker-compose -f docker-compose_flaskblog.yaml up -d
 sleep 3
