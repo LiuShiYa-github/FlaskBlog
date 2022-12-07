a1064f58a37610407ba39c8546181d684de4a3c3
热气球 <55818804+LiuShiYa-github@users.noreply.github.com>
2022-12-07 12:59:02 +0800

    Add files via upload

---
+++ docker-deploy/deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+cd ../../ && mv FlaskBlog/docker-deploy/* .
+wget http://nginx.org/download/nginx-1.18.0.tar.gz
+chmod +x init.sh
+docker build -t  flaskblog:v1.0  ./
+docker network create --subnet=172.32.0.0/24 flaskblog_net
+docker-compose -f docker-compose.yaml up -d
+docker exec -it mysql mysql -uroot -p123456 -e "use mysql; update user set host='%' where user ='root'; FLUSH PRIVILEGES;"
